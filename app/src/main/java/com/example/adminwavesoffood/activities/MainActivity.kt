838beb80eb68f2fb1ec518aacdf097e0bae320d0
Nguyễn Minh Trí <131839982+triengineervn@users.noreply.github.com>
2024-10-08 17:15:26 +0700

    implement backend firebase database: show info on menu

--- app/src/main/java/com/example/adminwavesoffood/activities/MainActivity.kt
+++ app/src/main/java/com/example/adminwavesoffood/activities/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.adminwavesoffood.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.adminwavesoffood.databinding.ActivityMainBinding
+import com.example.adminwavesoffood.models.OrdersModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +18,15 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private lateinit var database: FirebaseDatabase
+    private lateinit var userId: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        database = FirebaseDatabase.getInstance()
+        userId = FirebaseAuth.getInstance().currentUser!!.uid
 
         binding.btnAddMenu.setOnClickListener {
             val intent = Intent(this, AddMenuActivity::class.java)
@@ -49,5 +62,68 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        setTextViewOrderInfo()
+    }
+
+    private fun setTextViewOrderInfo() {
+        setOrderPendingText()
+        setOrderCompletedText()
+        setTotalPriceText()
+    }
+
+    private fun setOrderPendingText() {
+        val orderPendingRef = database.reference.child("users/${userId}/orders")
+        var orderPendingCount = 0
+        orderPendingRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                orderPendingCount = snapshot.childrenCount.toInt()
+                binding.orderPendingText.text = orderPendingCount.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                binding.orderPendingText.text = orderPendingCount.toString()
+                Toast.makeText(this@MainActivity, "Cannot load pending order", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
+    }
+
+    private fun setOrderCompletedText() {
+        val orderCompletedRef = database.reference.child("users/${userId}/completedOrders")
+        var orderCompletedCount = 0
+        orderCompletedRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                orderCompletedCount = snapshot.childrenCount.toInt()
+                binding.orderCompletedText.text = orderCompletedCount.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                binding.orderCompletedText.text = orderCompletedCount.toString()
+                Toast.makeText(this@MainActivity, "Cannot load completed order", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
+    }
+
+    private fun setTotalPriceText() {
+        val orderCompletedRef = database.reference.child("users/${userId}/completedOrders")
+        var orderCompletedPaid = mutableListOf<Int>()
+        orderCompletedRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (orderSnapshot in snapshot.children) {
+                    var completedOrder = orderSnapshot.getValue(OrdersModel::class.java)
+                    completedOrder?.totalPrice?.toIntOrNull().let { i ->
+                        orderCompletedPaid.add(i!!)
+                    }
+                }
+                binding.totalPriceText.text = orderCompletedPaid.sum().toString() + "$"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity, "Cannot load completed order", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
     }
 }
